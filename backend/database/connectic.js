d00f6e41ae51323d219eebba242f9d7015d753f6
sebastienbaquet <bas.envoi2604@gmail.com>
2024-03-22 10:19:57 +0100

    jointure table car

--- backend/database/connectic.js
+++ backend/database/connectic.js
@@ -1,6 +1,7 @@
 const connectic = [
   "insert into connectic(plugType) VALUES ('T2')",
   "insert into connectic(plugType) VALUES ('EF')",
+  "insert into connectic(plugType) VALUES ('ultra')",
 ];
 
 module.exports = connectic;
